4163260b3c08d3c611d47cc33b6c74dc9adaef22
Alex Nico <a.nicolaidis@outlook.com>
2022-11-01 08:59:04 +1100

    Completed Workout Routes

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -3,11 +3,39 @@ class WorkoutsController < ApplicationController
   end
 
   def new
+    @workout = Workout.new
+  end
+
+  def create
+    workout = Workout.create workout_params
+    @current_user.workouts << workout
+    redirect_to workout
   end
 
   def show
+    @workout = Workout.find params[:id]
   end
 
   def edit
+    @workout = Workout.find params[:id]
   end
+
+  def update
+    workout = Workout.find params[:id]
+    workout.update workout_params
+    redirect_to workout
+  end
+
+  def destroy
+    workout = Workout.find params[:id]
+    workout.destroy
+    redirect_to workouts_path
+  end
+
+
+  private 
+  def workout_params
+    params.require(:workout).permit(:date, :focus, :duration)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
 
 u1 = User.create(:email =>'Alex@ga.com',  :name =>'Alex', :height => 192, :weight =>80, :password => 'chicken')
 u2 = User.create(:email =>'Tom@ga.com',  :name =>'Tom', :height => 176, :weight =>60, :password => 'chicken')
@@ -18,3 +19,65 @@ u8 = User.create(:email =>'Peter@ga.com',  :name =>'Peter', :height => 151, :wei
 u9 = User.create(:email =>'Sara@ga.com',  :name =>'Sara', :height => 207, :weight =>100, :password => 'chicken')
 u10 = User.create(:email =>'Angela@ga.com',  :name =>'Angela', :height => 181, :weight =>51, :password => 'chicken')
 u11 = User.create(:email =>'Rory@ga.com',  :name =>'Rory', :height => 206, :weight =>105, :password => 'chicken')
+
+puts "#{User.count} Users Created"
+
+
+Workout.destroy_all
+
+w1 = Workout.create(:date => "2020-10-08", :focus => 'Arms', :duration => 1)
+w2 = Workout.create(:date => "2020-10-11", :focus => 'Back', :duration => 1.5)
+w3 = Workout.create(:date => "2020-10-15", :focus => 'Chest', :duration => 1)
+w4 = Workout.create(:date => "2020-10-16", :focus => 'Running', :duration => 0.5)
+w5 = Workout.create(:date => "2020-10-20", :focus => 'Legs', :duration => 1.5)
+w6 = Workout.create(:date => "2020-10-08", :focus => 'Arms', :duration => 1)
+w7 = Workout.create(:date => "2020-10-11", :focus => 'Back', :duration => 1.5)
+w8 = Workout.create(:date => "2020-10-15", :focus => 'Chest', :duration => 1)
+w9 = Workout.create(:date => "2020-10-16", :focus => 'Running', :duration => 1)
+w10 = Workout.create(:date => "2020-10-20", :focus => 'Legs', :duration => 1.5)
+
+puts "#{Workout.count} Workouts Created"
+
+Exercise.destroy_all
+
+# e1 = Exercise.create(:name => 'Bench Press', :weight => '', :repetitions => '', :sets => '', :distance => '', :duration => '')
+e1 = Exercise.create(:name => 'Bench Press', :weight => 50, :repetitions => 6, :sets => 3)
+e2 = Exercise.create(:name => 'Bench Press', :weight => 100, :repetitions => 1)
+e3 = Exercise.create(:name => 'Bench Press', :weight => 75, :repetitions => 4, :sets => 3)
+e4 = Exercise.create(:name => 'Bench Press', :weight => 120, :repetitions => 1)
+e5 = Exercise.create(:name => 'Push Ups', :weight => '', :repetitions => 50)
+e6 = Exercise.create(:name => 'Push Ups', :weight => '', :repetitions => 50, :sets => 2)
+
+e7 = Exercise.create(:name => 'Squats', :weight => 100, :repetitions => 1)
+e8 = Exercise.create(:name => 'Squats', :weight => 80, :repetitions => 5, :sets => 3)
+e9 = Exercise.create(:name => 'Squats', :weight => 90, :repetitions => 3, :sets => 2)
+
+e10 = Exercise.create(:name => '3km Run', :distance => 3, :duration => 12.5)
+e11 = Exercise.create(:name => '3km Run', :distance => 3, :duration => 12.5)
+e12 = Exercise.create(:name => '5km Run', :distance => 5, :duration => 23.5)
+e13 = Exercise.create(:name => '5km Run', :distance => 5, :duration => 26)
+
+puts "#{Exercise.count} Exercises Created"
+
+#Add workout to user profiles
+u1.workouts << w1 << w2 << w3 << w6 << w7 << w8
+u2.workouts << w5 << w10
+u3.workouts << w4 << w9
+
+
+#Add exercises to workouts
+w3.exercises << e1 << e2 << e5
+w8.exercises << e3 << e4 << e6
+
+w4.exercises << e10 << e12
+w4.exercises << e11 << e13
+
+w5.exercises << e7 << e8
+w10.exercises << e9
+
+
+
+
+
+
+
